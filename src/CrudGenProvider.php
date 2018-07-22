b17d8dc81e856ef9a8500fbc78ce87032e8af457
Kjoekjoe <tttttttman1@gmail.com>
2018-07-22 13:41:04 +0200

    updated autoload

--- src/CrudGenProvider.php
+++ src/CrudGenProvider.php
@@ -14,10 +14,28 @@ class CrudGenProvider extends ServiceProvider
      */
     public function boot()
     {
-        if ($this->app->runningInConsole()) {
-            $this->commands([
-                CrudGenerator::class,
-            ]);
+        if (is_dir(app_path().'/Modules/')) {
+            $modules = config("modules.enable") ?: array_map('class_basename', $this->files->directories(app_path().'/Modules/'));
+            foreach ($modules as $module) {
+                // Allow routes to be cached
+                if (!$this->app->routesAreCached()) {
+                    $route_files = [
+                        app_path() . '/Modules/' . $module . '/routes.php',
+                        app_path() . '/Modules/' . $module . '/routes/web.php',
+                        app_path() . '/Modules/' . $module . '/routes/api.php',
+                    ];
+                    foreach ($route_files as $route_file) {
+                        if ($this->files->exists($route_file)) {
+                            include $route_file;
+                        }
+                    }
+                }
+                $views  = app_path().'/Modules/'.$module.'/Views';
+
+                if ($this->files->isDirectory($views)) {
+                    $this->loadViewsFrom($views, $module);
+                }
+            }
         }
     }
 
